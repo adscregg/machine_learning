204bbff118348eb0328965c2c17f331c63887b9f
Anthony Scregg <git@adscregg.co.uk>
2019-07-25 10:04:02 +0100

    classifier with stemmed data, explored extracting num punc

--- code/analysis/reddit_NLP_analysis.py
+++ code/analysis/reddit_NLP_analysis.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import string
+import numpy as np
+import time
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style('whitegrid')
+
 import nltk
 from nltk.corpus import stopwords
-import numpy as np
-import seaborn as sns
-sns.set_style('whitegrid')
-import matplotlib.pyplot as plt
+from nltk.stem import PorterStemmer
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-import string
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
 
 
 reddit = pd.read_csv('../../datasets/reddit_train.csv')
@@ -25,14 +31,29 @@ print(reddit.describe(), '\n\n')
 print('Description of the data grouped by REMOVED: \n')
 print(reddit.groupby('REMOVED').describe(), '\n\n')
 
-print('Getting length of each comment...\n')
+print('Getting length of each comment...')
 reddit['LENGTH'] = reddit['BODY'].apply(len) # find the length of each comment
 print('done\n')
 
 reddit.hist(column = 'LENGTH', bins = 100, by = 'REMOVED', sharex = True)
+
+print('From this plot we can see that the length of the comment does not seem to be a good indicator of whether a message was removed or not, this is as expected but it is always worth exploring the data.\n')
 plt.show()
 
-print('From the plot that was shown, the length of the comment does not seem to be a good indicator of whether a message was removed or not, this is as expected but it is always worth exploring the data.\n')
+def extract_punc(text):
+    punc = [char for char in text if char in string.punctuation]
+    return len(punc)
+
+print('Getting number of punctuation points used in comments...')
+reddit['NUM_PUNC'] = reddit['BODY'].apply(extract_punc)
+print('done\n')
+
+reddit.hist(column = 'NUM_PUNC', bins = 100, by = 'REMOVED', sharex = True)
+
+print('Again this shows that there is little to no difference between the distribution of number of punctuation points used in comments that were removed and not removed meaning it is not a good indicator to add to our classifier.\n')
+plt.show()
+
+
 
 # It is important to preprocess your text data into a simpler form, for example removed words that carry no weight, e.g. 'I' or 'an' so as to not drown out the important words that do carry meaning, we will also remove punctuation from our comments, however this could be an important feature in some datasets.
 
@@ -41,31 +62,20 @@ def text_process(text):
     no_punc = ''.join(no_punc)
     return [word for word in no_punc.split() if word.lower() not in stopwords.words('english')]
 
-# print('Fitting and transforming using CountVectorizer...\n')
-#
-# bow_transformer = CountVectorizer(analyzer=text_process).fit(reddit['BODY'])
-# reddit_bow = bow_transformer.transform(reddit['BODY'])
-#
-# print('done\n')
-#
-# print('Shape: ', reddit_bow.shape, '\n\n')
-#
-# print('Fitting TfidfTransformer...')
-# tfidf_transformer = TfidfTransformer().fit(reddit_bow)
-# print('Trasforming using TfidfTransformer...')
-# reddit_tfidf = tfidf_transformer.transform(reddit_bow)
 
-# reddit['REMOVED'] = reddit['REMOVED'].map({'No': 0, 'Yes': 1})
-
+print('Splitting data into train and test set...')
 X_train, X_test, y_train, y_test = train_test_split(reddit['BODY'], reddit['REMOVED'])
-#
+print('done\n')
+
 text_pipeline = Pipeline([('bow', CountVectorizer(analyzer=text_process)),
                             ('tfidf', TfidfTransformer()),
                             ('NaiveBayes', MultinomialNB())
                         ])
-#
+print('Fitting NaiveBayes model...')
 remove_analysis = text_pipeline.fit(X_train, y_train)
+print('done\nPredicting values...')
 pred = remove_analysis.predict(X_test)
+print('done\n')
 
 print(accuracy_score(y_test, pred), '\n\n')
 print(classification_report(y_test, pred), '\n\n')
@@ -82,10 +92,15 @@ reddit = pd.concat([reddit_no.iloc[:to_select,:], reddit_yes.iloc[:to_select,:]]
 sns.countplot(reddit['REMOVED'])
 plt.show()
 
+print('Splitting data into train and test set...')
 X_train, X_test, y_train, y_test = train_test_split(reddit['BODY'], reddit['REMOVED'])
+print('done\n')
 
+print('Fitting NaiveBayes model with balanced dataset...')
 remove_analysis_balanced = text_pipeline.fit(X_train, y_train)
+print('done\nPredicting values...')
 pred_balanced = remove_analysis_balanced.predict(X_test)
+print('done\n')
 
 print(accuracy_score(y_test, pred_balanced), '\n\n')
 print(classification_report(y_test, pred_balanced), '\n\n')
@@ -93,3 +108,34 @@ print(confusion_matrix(y_test, pred_balanced), '\n\n')
 
 
 print('Although the accuracy of the model is approximitely equal to that of the previous one, the overall performance metrics are much better as the model predicts more evenly about the classes. This is demonstrative as to why you need to explore your data before blindly fitting a model to it and looking at only the accuracy score as this can paint a very false picture of how good your model is. The phrase \'Garbage in garbage out comes to mind here\'\n')
+print('To make the predictions better, another thing that might work is stemming. This is the process of attempting to reduce a word down to it\'s base word, e.g. running would become run. Lets try this and see if it has an impact on our predictions.(we will use the same balanced dataset as the previous model)\n')
+
+def text_process_with_stem(text):
+    ps = PorterStemmer()
+    no_punc = [char for char in text if char not in string.punctuation]
+    no_punc = ''.join(no_punc)
+    no_stops = [word for word in no_punc.split() if word.lower() not in stopwords.words('english')]
+    words_ps = [ps.stem(word.lower()) for word in no_stops]
+    return words_ps
+
+text_pipeline_stem = Pipeline([('bow', CountVectorizer(analyzer=text_process_with_stem)),
+                            ('tfidf', TfidfTransformer()),
+                            ('NaiveBayes', MultinomialNB())
+                        ])
+
+print('Splitting data into train and test set...')
+X_train, X_test, y_train, y_test = train_test_split(reddit['BODY'], reddit['REMOVED'])
+print('done\n')
+
+print('Fitting NaiveBayes model with balanced and stemmed dataset...')
+remove_analysis_balanced_stem = text_pipeline_stem.fit(X_train, y_train)
+print('done\nPredicting values...')
+pred_balanced_stem = remove_analysis_balanced_stem.predict(X_test)
+print('done\n')
+
+
+print(accuracy_score(y_test, pred_balanced_stem), '\n\n')
+print(classification_report(y_test, pred_balanced_stem), '\n\n')
+print(confusion_matrix(y_test, pred_balanced_stem), '\n\n')
+
+print('This model performs very simmilar to the previous model, it is possibly marginally better but this will likely depend on how the data is split.')
